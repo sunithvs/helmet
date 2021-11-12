aca13c34e8844c4ca0fc0b2090835b83b35c7f1b
sunith <sunithvs2002@gmail.com>
2021-11-12 13:18:29 +0530

    remover django jet

--- drfTemplate/urls.py
+++ drfTemplate/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('', admin.site.urls),
-    path('jet/', include('jet.urls', 'jet')),
 ]
